522842c51e9b2a6e64fb76e81abe741ce9b789f6
Ben Schreck <bschreck@mit.edu>
2014-04-25 16:32:54 -0400

    added bitch and molly to words

--- jChange.py
+++ jChange.py
@@ -1,8 +1,9 @@
 from os import system
+#import pyttsx
 
 def joReplace(song):
     #you = re.compile('you', re.IGNORECASE)
-    you = re.compile('you|(baby)|honey|babe|she|woman|girl|lady', re.IGNORECASE)
+    you = re.compile('you|molly|bitch|(baby)|honey|babe|she|woman|girl|lady', re.IGNORECASE)
 
     your = re.compile('your|You\\\\\'re|her', re.IGNORECASE)
     for line in song:
@@ -10,10 +11,24 @@ def joReplace(song):
             if your.match(line[index]):
                 line[index] = 'Joanna\\\'s'
             if you.match(line[index]):
-                line[index] = 'The Joannas'
+                line[index] = 'Joanna'
     return song
 
 def txtToSpeech(song):
+    #engine = pyttsx.init()
+    #engine.setProperty('rate', 70)
+    #voices = engine.getProperty('voices')
+    #for voice in voices:
+        #print "Using voice:", repr(voice)
+        #engine.setProperty('voice', voice.id)
+        #engine.say("Hi there, how's you ?")
+        #engine.say("A B C D E F G H I J K L M")
+        #engine.say("N O P Q R S T U V W X Y Z")
+        #engine.say("0 1 2 3 4 5 6 7 8 9")
+        #engine.say("Sunday Monday Tuesday Wednesday Thursday Friday Saturday")
+        #engine.say("Violet Indigo Blue Green Yellow Orange Red")
+        #engine.say("Apple Banana Cherry Date Guava")
+    #engine.runAndWait()
     for line in song:
         for word in line:
             system('say ' + word)
